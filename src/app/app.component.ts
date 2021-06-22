594049f35973f00a6a548521e86ca9210d9f6ada
JulioFuentesBarrera <JulioFuentesBarrera@101061PC>
2021-06-22 07:51:51 -0500

    e guar da vriable video 31 udemi angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'MiPrimeraApp';
+  saludo = "Hola angular como esta?";
+
+  peliculasEnCine: { titulo: string; fechaLanzamiento: Date; precio: number; }[] | undefined;
+  peliculasProximosEstrenos: { titulo: string; fechaLanzamiento: Date; precio: number; }[] | undefined;
+
+
+  ngOnInit(): void {
+
+
+    setTimeout(() => {
+
+      this.peliculasEnCine = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1490
+
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300
+
+      }];
+    }, 3000);
+
+    // peliculasEnCIne;
+    this.peliculasProximosEstrenos=[];
+
+    // this.peliculasProximosEstrenos = [{
+    //   titulo: 'Spider-Man',
+    //   fechaLanzamiento: new Date(),
+    //   precio: 1490
+
+    // },
+    // {
+    //   titulo: 'Moana',
+    //   fechaLanzamiento: new Date('2016-11-14'),
+    //   precio: 300
+
+    // },
+    // {
+    //   titulo: 'Piter pan',
+    //   fechaLanzamiento: new Date('2016-11-14'),
+    //   precio: 300
+
+    // },
+    // {
+    //   titulo: 'Piter pan2',
+    //   fechaLanzamiento: new Date('2016-11-14'),
+    //   precio: 300
+
+    // },
+    // {
+    //   titulo: 'Piter pan3',
+    //   fechaLanzamiento: new Date('2016-11-14'),
+    //   precio: 300
+
+    // }];
+
+
+
+
+  }
+
+}
+
+
+
+// export class AppComponent implements OnInit {
+//   ngOnInit(): void {
+
+
+//    this.peliculas = [{
+//       titulo: 'Spider-Man',
+//       fechaLanzamiento: new Date(),
+//       precio: 1400.99
+//     },
+//     {
+//       titulo: 'Moana',
+//       fechaLanzamiento: new Date('2016-11-14'),
+//       precio: 300.99
+//     }]
+//   }
+
+
+//   peliculas: { titulo: string; fechaLanzamiento: Date; precio: number; }[] | undefined;
+
+
+
+
+
+//   title = 'Al valor que yo quiera';
+//   ocultar = false;
+//   peliculasEnCines=[];
+
+// }
+
---
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  templateUrl: './listado-peliculas.component.html',
+  styleUrls: ['./listado-peliculas.component.css']
+})
+export class ListadoPeliculasComponent implements OnInit {
+
+
+  constructor() { }
+  @Input()
+  peliculas: { titulo: string; fechaLanzamiento: Date; precio: number; }[] | undefined;
+
+  ngOnInit(): void {
+
+  }
+
+  //remover las peliculas una a una 
+  remover(indicePelicula: number): void {
+    this.peliculas?.splice(indicePelicula,1);
+    //   //this.peliculas.splice(indicePelicula, 1);
+    }
+
+}
+
