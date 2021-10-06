1e46aea3efabb45fcdadfd9f91cb3d3fb29c59b1
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2021-10-06 08:49:24 -0500

    video 67 selector de actores-arrastrar elemento

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -13,6 +13,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import  {MatAutocompleteModule} from '@angular/material/autocomplete'
 import { MatTableModule} from '@angular/material/table'
+import {DragDropModule} from '@angular/cdk/drag-drop';//componente de arrastrar
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { MatTableModule} from '@angular/material/table'
     MatTabsModule,
     MatAutocompleteModule,
     MatTableModule,
+    DragDropModule,
   ],
   imports: [CommonModule],
 })
